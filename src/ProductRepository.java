3e292a0ae7df730b3a3cc4c93f751c769a52d11f
Bezdomnii <73344097+rejiculus@users.noreply.github.com>
2024-04-27 19:55:43 +0300

    Add product capacity
    change treeSet to treeMap<Proudct,Integer>

--- src/ProductRepository.java
+++ src/ProductRepository.java
@@ -1,27 +1,33 @@
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class ProductRepository {
-    private static TreeSet<Product> products;
+    private static TreeMap<Product,Integer> products;
 
     static {
-        products=new TreeSet<>();
-        products.add(new Product("soup",ProductType.HOT_FOOD,100.0,false));
-        products.add(new Product("tea",ProductType.HOT_DRINK,50.0,true));
-        products.add(new Product("coffee",ProductType.HOT_DRINK,150.0,true));
-        products.add(new Product("cake",ProductType.FOOD,300.0,true));
-        products.add(new Product("sidr",ProductType.ALCOHOL,200.0,true));
+        products=new TreeMap<>();
+        products.put(new Product("soup",ProductType.HOT_FOOD,100.0,false),10);
+        products.put(new Product("tea",ProductType.HOT_DRINK,50.0,true),10);
+        products.put(new Product("coffee",ProductType.HOT_DRINK,150.0,true),10);
+        products.put(new Product("cake",ProductType.FOOD,300.0,true),10);
+        products.put(new Product("sidr",ProductType.ALCOHOL,200.0,true),10);
 
     }
-    public static Set<Product> getAll(){
+    public static Map<Product,Integer> getAll(){
         return products;
     }
+
     public static Product getByName(String name){
-        for(Product p:products){
+        for(Product p:products.keySet()){
             if(name.equals(p.getName()))
                 return p;
         }
         return null;
+    }
+    public static int getCount(Product product){
+        return products.get(product);
     }
 
 }
