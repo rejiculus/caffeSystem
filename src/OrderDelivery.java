b72e2e181902d79f3fa2a47f0a4fad909e9b5ec9
Bezdomnii <73344097+rejiculus@users.noreply.github.com>
2024-04-27 17:50:06 +0300

    Create order delivery class
    include place(table) where we have to deliver order

--- src/OrderDelivery.java
+++ src/OrderDelivery.java
@@ -1,3 +1,31 @@
-public enum OrderDelivery {
+import java.util.Objects;
 
+public class OrderDelivery {
+    private int tableNumber;
+    OrderDelivery(int table){
+        if(table<0||table>CaffeParams.TABLE_COUNT)
+            throw new RuntimeException("Incorrect table");
+        tableNumber=table;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderDelivery that = (OrderDelivery) o;
+        return tableNumber == that.tableNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tableNumber);
+    }
 }
