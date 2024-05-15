36873fe4f2b676834cbeeecc55da0c438761d24d
bezVSC <nik.bezdomnii@gmail.com>
2024-05-15 11:59:59 +0300

    Add new exceptions

--- src/common_exceptions/ContainsException.java
+++ src/common_exceptions/ContainsException.java
@@ -1,6 +1,16 @@
 package common_exceptions;
 
 public class ContainsException  extends RuntimeException{
+    public ContainsException(Long id){
+        super(String.format(
+            "Something wrong: in order %d reserved less products then have to delete!",id));
+    }
+    public ContainsException(String product){
+        super(String.format("It is not contains %s!",product));
+    }
+    public ContainsException(String product,int count){
+        super(String.format("It is contains %s less than %d!",product,count));
+    }
     public ContainsException(Long id, String product){
         super(String.format("Order - %d is not contains %s!",id,product));
     }
--- src/common_exceptions/LessThanZeroException.java
+++ src/common_exceptions/LessThanZeroException.java
@@ -1,6 +1,9 @@
 package common_exceptions;
 
 public class LessThanZeroException extends RuntimeException{
+    public LessThanZeroException(){
+        super("Value can't be less then zero!");
+    }
     public LessThanZeroException(Long id){
         super(String.format("Value can't be less then zero! Order - %d",id));
     }
